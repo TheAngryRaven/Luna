978ca9306516f2c16dcb5df3c44c3c7ee7e07587
CrimsonDove <mikeachampagne@gmail.com>
2016-04-23 22:40:26 -0400

    some crappy style changes until i devote time to a better theme

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -19,6 +19,16 @@ $sessionKeys = Session::get("serverKeys");
 
     <script src="{{ URL::asset('bootstrap/js/bootstrap.min.js') }}"></script>
     <script src="{{ URL::asset('bootstrap/bootbox.min.js') }}"></script>
+
+
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ URL::asset('favicon') }}/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="{{ URL::asset('favicon') }}/apple-touch-icon-152x152.png" />
+    <link rel="icon" type="image/png" href="{{ URL::asset('favicon') }}/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="{{ URL::asset('favicon') }}/favicon-16x16.png" sizes="16x16" />
+    <meta name="application-name" content="LunarMessaging.net"/>
+    <meta name="msapplication-TileColor" content="#3B3B3B" />
+    <meta name="msapplication-TileImage" content="{{ URL::asset('favicon') }}/mstile-144x144.png" />
+
 </head>
 <body>
 
@@ -27,6 +37,7 @@ $sessionKeys = Session::get("serverKeys");
 
 
     <!-- Main -->
+    <img style="width: 90%; max-width:387px;margin-bottom:2em;" src="{{ URL::asset('header.png') }}" alt="LunarMessaging Logo">
     <section id="main">
         <div id="encryptedContent"><p style="margin: 0;">Please Wait<br>Generating encryption keys</p></div>
     </section>
